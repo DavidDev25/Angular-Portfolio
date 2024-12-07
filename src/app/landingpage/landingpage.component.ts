9d8a9e1147a3d8a32e81d022c63731af309b3db0
DavidDev25 <david.werner@david-werner.dev>
2024-12-07 21:10:29 +0100

    added MySkillsSection, replaced some Skills, added new Skill, added hover to challenge me

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
+import { WhyMeComponent } from './why-me/why-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [HeroComponent],
+  imports: [HeroComponent, WhyMeComponent, MySkillsComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss',
 })
