5484444747be7c23c6a519fa19fe30b73d25e638
DavidDev25 <david.werner@david-werner.dev>
2024-12-07 17:24:18 +0100

    replaced  front icons and added new icon.svg

---
+++ src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [NavbarComponent],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss',
+})
+export class LandingpageComponent {}
