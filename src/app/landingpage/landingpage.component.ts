222390c97c17a01d9762f606a589787909a7c2ff
DavidDev25 <david.werner@david-werner.dev>
2024-12-07 18:09:44 +0100

    added navbar to shared folder, adjust path for running state

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [NavbarComponent],
+  imports: [],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss',
 })
