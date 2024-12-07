4d91e6b3adf927d12f4aef0e5a0eafccaa3b1e42
DavidDev25 <david.werner@david-werner.dev>
2024-12-07 19:17:02 +0100

    added hero section

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss',
 })
