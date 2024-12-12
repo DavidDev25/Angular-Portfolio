96fea2fb48a88c49aa65ff825240416c94cc7b0b
DavidDev25 <david.werner@david-werner.dev>
2024-12-12 16:48:54 +0100

    added my Project and Skills translation

--- src/app/landingpage/my-projects/my-projects.component.ts
+++ src/app/landingpage/my-projects/my-projects.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-my-projects',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './my-projects.component.html',
-  styleUrl: './my-projects.component.scss'
+  styleUrl: './my-projects.component.scss',
 })
-export class MyProjectsComponent {
-
-}
+export class MyProjectsComponent {}
