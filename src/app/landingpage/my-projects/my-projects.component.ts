0952505105ef87b52756a633b2be29259df42caa
DavidDev25 <david.werner@david-werner.dev>
2024-12-16 09:35:57 +0100

    added redirection to Github and Live Test

--- src/app/landingpage/my-projects/my-projects.component.ts
+++ src/app/landingpage/my-projects/my-projects.component.ts
@@ -7,4 +7,22 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss',
 })
-export class MyProjectsComponent {}
+export class MyProjectsComponent {
+  redirectJoin() {
+    window.open('https://github.com/DavidDev25/DA-Join');
+  }
+
+  redirectJoinLive() {
+    window.open('http://david-werner.developerakademie.net/Join/index.html');
+  }
+
+  redirect() {
+    window.open('https://github.com/DavidDev25/El-Pollo-Loco', 'blank');
+  }
+
+  redirectPolloLive() {
+    window.open(
+      'http://david-werner.developerakademie.net/El_Pollo_Loco/index.html'
+    );
+  }
+}
